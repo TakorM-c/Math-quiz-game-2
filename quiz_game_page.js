9dd3cd769bb82a63737d9c15cbc3d46e3c60a1b5
TakorM-c <83616091+TakorM-c@users.noreply.github.com>
2022-01-01 15:39:13 -0500

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -8,3 +8,18 @@ document.getElementById("player1_score").innerHTML=player1_score;
 document.getElementById("player2_score").innerHTML=player2_score;
 document.getElementById("player_question").innerHTML="question turn -"+ player1_name;
 document.getElementById("player_answer").innerHTML="answer turn -"+ player2_name;
+function send(){
+    number1 = document.getElementById("number1").ariaValueMax;
+    number2 = document.getElementById("number2").ariaValueMax;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    question_number = "<h4>" + number1 + "X"+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type='text' id= 'input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onlick='check()'></button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
+function check(){
+    if()
+}
