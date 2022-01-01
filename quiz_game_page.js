fd3b1989c0aadf4a3850691bb3688a89f1f9161e
TakorM-c <83616091+TakorM-c@users.noreply.github.com>
2022-01-01 15:51:57 -0500

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,3 +1,5 @@
+var question_turn = "player1";
+var answer_turn = "player2";
 player1_name=localStorage.getItem("player1_name");
 player2_name=localStorage.getItem("player2_name");
 player1_score=0;
@@ -21,5 +23,28 @@ function send(){
     document.getElementById("number2").value = "";
 }
 function check(){
-    if()
-}
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+        update_player1_score = player1_score +1;
+        document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+     else
+     {
+         update_player2_score = player2_score +1;
+         document.getElementById("player2_score").innerHMTL = update_player2_score;
+     }    
+    }
+    if(question_turn == "player1")
+    {
+        question_turn = "player2"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+
+    }
+    else
+    {
+        question_turn = "player1"
+        document.getElementById("player_question").innerHTML= "Question Turn - " + player1_name;
+    }
